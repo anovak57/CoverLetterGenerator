c4810b50a251c1b419cf2f9206be59822d366d3a
anovak57 <ante.novak408@gmail.com>
2024-07-11 12:29:00 +0200

    Create login and register page

---
+++ CLIENT/CoverLetterGeneratorClient/src/app/auth/auth.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-auth',
+  standalone: true,
+  imports: [CommonModule, FormsModule],
+  templateUrl: './auth.component.html',
+  styleUrls: ['./auth.component.css']
+})
+export class AuthComponent {
+  isSignUp = false;
+
+  togglePanel() {
+    this.isSignUp = !this.isSignUp;
+  }
+}
